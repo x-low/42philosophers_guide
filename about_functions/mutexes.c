5b029c7a763f7b81202a26baec2705e502e7af6b
xlow <lowxf7@gmail.com>
2024-10-18 03:18:19 +0800

    Added initial comments for mutexes.c

---
+++ about_functions/mutexes.c
@@ -0,0 +1,9 @@
+#include "libraries.h"
+
+// PART 3/3
+// Mutex functions: init, lock, unlock, destroy
+
+// !! MUTEXES ONLY MAKE SENSE IF YOU UNDERSTAND THREADS !!
+
+// common problems e.g data races/deadlock will be discussed in next section
+// here we will discuss how threads behave with mutexes
